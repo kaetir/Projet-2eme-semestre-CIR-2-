cea9416dd20249c64639e0d1afe6366c7f049775
Je suis Louis avec un smocking <canalbloglouissama@gmail.com>
2018-02-09 14:58:28 +0100

    Update table

--- tabFile.js
+++ tabFile.js
@@ -1,5 +1,9 @@
 var Tab = [
-	[1,2,3],
-	[4,5,6],
+	["Bosseur","Suceptible","Gaffeur","Manipulateur"],
+	["n-1","(n-1)-t","(n-1)-random(n-1)","(n-1)+choix"],
+]
 
+var Tab2 = [
+	["BOSSEUR","SUCEPTIBLE","GAFFEUR","MANIPULATEUR","OSEF","RANDOM","LEADER","PEUREUX"],
+	["n-1","(n-1)-t","(n-1)-random(n-1)","(n-1)+choix","0","random(n-1)","(n-1)+Pts bonus","(n-1)-somme/n-1"],
 ]
