c7d5bb26f086bb882025a1b77d1ccd232a6714a2
Vanacker Anaïs <anais.vanacker@isen.yncrea.fr>
2018-02-16 16:07:36 +0100

    add favicon

--- index.php
+++ index.php
@@ -18,7 +18,18 @@
  	}
  </style>
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+ 
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
+
+</head> 
 <body>
 	<div class="w3-container w3-margin-top " >
 		<?php 
