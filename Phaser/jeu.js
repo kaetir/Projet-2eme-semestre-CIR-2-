b2c3d18ae9496b33346c1b711a0b423d31c75ea2
Desprez Thibault <thibault.desprez1@gmail.com>
2018-03-12 13:55:57 +0100

    downgrade phaser and add a button exemple
    phaser3 is not ready to use

--- Phaser/jeu.js
+++ Phaser/jeu.js
@@ -1,27 +1,10 @@
 
-var config = {
-      type: Phaser.AUTO,
-      width: 640,
-      height: 480,
-      parent: "jeu",
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 200 }
-        }
-      },
-      scene: {
-        preload: preload,
-        create: create
-      }
-    };
-
-var game = new Phaser.Game(config);
-
+var game = new Phaser.Game(680, 480, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
 function preload() {
 
-    this.load.spritesheet('button', 'Phaser/png/bouton_de_tes_morts.png', 250, 100);
+    game.load.spritesheet('button', 'png/bouton_de_tes_morts.png', 250, 100);
+
 }
 
 var button;
@@ -29,8 +12,11 @@ var background;
 
 function create() {
 
+    game.stage.backgroundColor = '#182d3b';
 
-    button = this.add(Phaser.button(100, 400, 'button', actionOnClick, this, 2, 1, 0));
+    background = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+    button = game.add.button(game.world.centerX - 125, game.world.centerY- 50, 'button', actionOnClick, this, 2, 1, 0);
 
     button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
