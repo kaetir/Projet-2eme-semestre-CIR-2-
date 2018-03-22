312cf28b60e34da30df5d3c84746942d5140b4d7
Je suis Louis avec un smoking <canalbloglouissama@gmail.com>
2018-03-22 11:10:48 +0100

    Commentaires sur le personnage.js
    Mise en place de commentaire.

--- algo/memoire/personnage.js
+++ algo/memoire/personnage.js
@@ -1,11 +1,22 @@
 define(["coups","game"], function(coup,game){
     class Personnage {
     	static this.nbJoueur = 0;
-    	constructor(nom,type){
-    		this.type = type; 			//string
-    		if(type == "Suceptible"){
-    			this.TRIGERED = [];
-    			this.I_AM_TRIGERED = function(&perso){
+    	
+        constructor(nom,type){
+    		
+            this.type = type; 			//string
+    		
+            if(type == "Suceptible"){
+    			
+                this.TRIGERED = [];/*Tableau récapitulatif des persos qui ont l'on BETRAY*/
+    			
+                /*
+                description : Fonction qui push l'id d'un perso dans le tableau TRIGERED.
+                argument : pointeur sur perso.
+                return :NADA
+                */
+
+                this.I_AM_TRIGERED = function(&perso){
     				this.TRIGERED.push(this.id);
     			}
     		}
@@ -17,9 +28,9 @@ define(["coups","game"], function(coup,game){
     	}
     	
     	/*
-    	description :
-    	argument :
-    	return :
+    	description : Affectation mémoire du coup donnée et reçu.
+    	argument : donne, recu, et un personnage.
+    	return :NADA
     	 */
     	
     	trade(donnee, recu, &perso){
@@ -29,19 +40,33 @@ define(["coups","game"], function(coup,game){
     		}
     	}
 
+        /*
+        description : Fonction test, mémoire vide ou non.
+        argument : nada
+        return :Bool true = mémoire vide OU false = mémoire utilisé.
+         */
     	
     	memoire_is_empty(){
     		return (this.memoire == []);
     	}
 
-
+        /*
+        description : Algo d'échange entre 2 personnages.
+        argument : Pointeur sur le type du perso.
+        return : Bool true ou false selon les persos.
+         */
 
 
     	donne_for_trade($perso){
 
     		switch (this.type) {
-    		 	
+
     		 	case "Manipulateur":
+
+                    /*Liste d'échanges: COOP >> BETRAY >> ADAPTATION*/
+
+                    /*Affectation des variables last_turn et last_last_turn selon le tour en cour.*/
+
     		 		if(game.tour > 1){
     		 			var last_turn = this.memoire.filter(coup=>coup.tour == game.tour-1)
     		 			if(game.tour > 2){
@@ -49,13 +74,18 @@ define(["coups","game"], function(coup,game){
     		 			}
     		 		}
     		 		
-    		 		if(this.memoire_is_empty == true){
+                    /*Premier échange avec l'individu.*/
+    		 		
+                    if(this.memoire_is_empty == true){
     				return true;
     				}
     				else{
-    					if(game.tour = 2){
+    					if(game.tour = 2){/*Deuxième échange avec l'individu.*/
     					return false;
     				}
+
+                    /*Après le deuxième tour, le manipulateur fait des choix adaptatifs.*/
+
     				else{
     					if(last_turn == true && last_last_turn == true){
     						return false;
@@ -68,11 +98,19 @@ define(["coups","game"], function(coup,game){
     		 		break;
     		 		
     		 	case "Suceptible":
+
+                /*Liste d'échanges: COOP >> ADAPTATION >> ADAPTATION*/
+
+                    /*Premier échange avec l'individu.*/
+
     		 		if(memoire_is_empty == true){
     					return true;
     				}
+
+                    /*Reste des échanges avec l'individu.*/
+
     				else{
-    					if(TRIGERED.includes(this.id)){
+    					if(TRIGERED.includes(this.id)){/*On teste si l'id du perso se trouve dans le tableau TRIGERED*/
     						return false;
     					}
     					else{
@@ -83,15 +121,24 @@ define(["coups","game"], function(coup,game){
     		 		break;
     		 		
     		 	case "Bosseur":
+
+                    /*Liste d'échanges: COOP >> COOP >> COOP*/
+
     		 		return true;
 
     		 		break;
 
     		 	case "Gaffeur":
+
+                    /*Liste d'échanges: RANDOM >> RANDOM >> RANDOM*/
+
     		 		return (int(Math.random()*100)>33);  //33 est le % de chances d'echec
 
     		 		break;
-    		 	default:
+    		 	
+
+
+                default:/*Si on a un type autre que ceux précédement on renvoie en console un message d'erreur.*/
     		 		console.log('ERREUR DE TYPE')
     		 		break;
     		 }
