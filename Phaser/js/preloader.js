1cccde7d50ab6aac112e6fd877324bb5e2a9e7d2
Desprez Thibault <thibault.desprez1@gmail.com>
2018-04-06 15:01:03 +0200

    un peu d'organisation et d'annimation

--- Phaser/js/preloader.js
+++ Phaser/js/preloader.js
@@ -13,13 +13,13 @@ function preload() {
     /*this.load.audio('audio-bounce', ['audio/bounce.ogg', 'audio/bounce.mp3', 'audio/bounce.m4a']);*/
 
     
-    game.load.atlas('gaffeur', 'asset/img/gaffeur_sprite.png', 'asset/img/gaffeur_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-
-    
-    game.load.atlas('bouton', 'asset/img/boutons_sprite.png', 'asset/img/boutons_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    game.load.atlas('gaffeur', 'assets/sprite_sheet/gaffeur_sprite.png', 'assets/sprite_sheet/json/gaffeur_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    game.load.atlas('bouton', 'assets/sprite_sheet/boutons_sprite.png', 'assets/sprite_sheet/json/boutons_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 
 
-    /*this.game.load.atlasJSONArray('boutons_sprite', 'asset/img/boutons_sprite.png', "asset/img/boutons_sprite.json");
+
+
+    /*this.game.load.atlasJSONArray('boutons_sprite', 'assets/sprite_sheet/boutons_sprite.png', "assets/sprite_sheet/boutons_sprite.json");
     
     sprite = this.game.add.sprite(100, 180, 'boutons_sprite', 'bouton_arcade');
     
@@ -31,17 +31,21 @@ function preload() {
 
 function create() {
     
-    var bouton = this.game.add.button(this.game.world.centerX - 250, 400,"bouton", actionOnClick, this,);
-    
-    bouton.onInputOver.add(over, this);
-    bouton.onInputOut.add(out, this);
 
     var billy =game.add.sprite(0,0,"gaffeur");
     billy.scale.setTo(0.5,0.5);
     billy.anchor.setTo(0,0);
+    
     billy.animations.add('jump');
-    billy.animations.play('jump', 20, true);
+    billy.animations.play('jump', 12, true);
 
+
+    var bouton = this.game.add.button(this.game.world.centerX - 250, 400,"bouton", actionOnClick, this,);
+    bouton.scale.setTo(0.5,0.5);
+    bouton.setFrames(2,0,1);
+
+    bouton.onInputOver.add(over, this);
+    bouton.onInputOut.add(out, this);
 
     function over() {
         console.log('button over');
