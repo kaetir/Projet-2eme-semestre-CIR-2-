40d8cc46d8c31adf0822a1e89b8f4fe956331572
Desprez Thibault <thibault.desprez1@gmail.com>
2018-04-13 15:17:37 +0200

    update menu

--- Phaser/js/main_menu.js
+++ Phaser/js/main_menu.js
@@ -1,43 +1,40 @@
+var main_menu = {
+    Billy   : {},
+    Teemo   : {},
+    Jeanne  : {},
+    Ed      : {},   
 
-function main_menu (){
+    create : function(){
 
+        var Billy = game.add.sprite(25,100,"bosseur");
+        var Teemo = game.add.sprite(225,100,"gaffeur");
+        var Jeanne = game.add.sprite(425,100,"susceptible");
+        var Ed = game.add.sprite(625,100,"manipulateur");
 
+        Billy.scale.setTo(0.5,0.5);
+        Teemo.scale.setTo(0.5,0.5);
+        Jeanne.scale.setTo(0.5,0.5)
+        Ed.scale.setTo(0.5,0.5);
 
-    var Billy = game.add.sprite(25,100,"bosseur");
-    var Teemo = game.add.sprite(225,100,"gaffeur");
-    var Jeanne = game.add.sprite(425,100,"susceptible");
-    var Ed = game.add.sprite(625,100,"manipulateur");
-    
+        Billy.animations.add('jump');
+        Billy.animations.play('jump', 12, true);
 
-    Billy.scale.setTo(0.5,0.5);
-    Teemo.scale.setTo(0.5,0.5);
-    Jeanne.scale.setTo(0.5,0.5)
-    Ed.scale.setTo(0.5,0.5);
+        Teemo.animations.add('jump');
+        Teemo.animations.play('jump', 12, true);
 
+        Jeanne.animations.add('jump');
+        Jeanne.animations.play('jump', 12, true);
 
-    Billy.animations.add('jump');
-    Billy.animations.play('jump', 12, true);
+        Ed.animations.add('jump');
+        Ed.animations.play('jump', 12, true);
 
-    Teemo.animations.add('jump');
-    Teemo.animations.play('jump', 12, true);
-    
-    Jeanne.animations.add('jump');
-    Jeanne.animations.play('jump', 12, true);
+        var arcade = game.add.button(game.world.centerX - 250 - 100, game.world.centerY +100 ,"boutons", startArcade, this);
+        arcade.scale.setTo(0.5,0.5);
+        arcade.setFrames(2,0,1);
 
-    Ed.animations.add('jump');
-    Ed.animations.play('jump', 12, true);
-    
-    
-
-
-
-    var arcade = game.add.button(game.world.centerX - 250 - 100, game.world.centerY +100 ,"boutons", startArcade, this);
-    arcade.scale.setTo(0.5,0.5);
-    arcade.setFrames(2,0,1);
-
-    var histoire = game.add.button(game.world.centerX +100 , game.world.centerY +100 ,"boutons", startHistory);
-    histoire.scale.setTo(0.5,0.5);
-    histoire.setFrames(11,9,10);
+        var histoire = game.add.button(game.world.centerX +100 , game.world.centerY +100 ,"boutons", startHistory);
+        histoire.scale.setTo(0.5,0.5);
+        histoire.setFrames(11,9,10);
 
 
     /*
@@ -71,8 +68,6 @@ function main_menu (){
         game.state.add("game", leJeu);
         game.state.start('game');
     };
-
-
     /*
     description : nettoie la scene et lance le mode histoire
     argument : none
@@ -81,6 +76,9 @@ function main_menu (){
     function startHistory (){
 
     };
+} 
+
+
 
 
 }
