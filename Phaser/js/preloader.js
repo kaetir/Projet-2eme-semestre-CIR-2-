0fc80046344f7fe01ade95b20b01058c4809641d
Je suis Louis avec un smoking <canalbloglouissama@gmail.com>
2018-05-12 13:53:33 +0200

    Importation des bruitages + code préolader

--- Phaser/js/preloader.js
+++ Phaser/js/preloader.js
@@ -44,13 +44,21 @@ function preload () {
 function load() {
 
 	game.load.image("logo", "assets/logo/trustMe_logo_app.png");
+	
 	game.load.audio('boden', ['assets/sound/Ambiance_1.ogg']);
+	game.load.audio('Betray_1',['assets/sound/Betray_1.ogg']);
+	game.load.audio('Betray_2',['assets/sound/Betray_2.ogg']);
+	game.load.audio('Coop_1',['assets/sound/Coop_1.ogg']);
+	game.load.audio('Coop_2',['assets/sound/Coop_2.ogg']);
+
+
 	game.load.atlas('gaffeur', 'assets/sprite_sheet/gaffeur_sprite.png', 'assets/sprite_sheet/json/gaffeur_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	game.load.atlas('bosseur', 'assets/sprite_sheet/bosseur_sprite.png', 'assets/sprite_sheet/json/bosseur_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	game.load.atlas('susceptible', 'assets/sprite_sheet/susceptible_sprite.png', 'assets/sprite_sheet/json/susceptible_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	game.load.atlas('manipulateur', 'assets/sprite_sheet/manipulateur_sprite.png', 'assets/sprite_sheet/json/manipulateur_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	game.load.atlas('mec', 'assets/sprite_sheet/mec_sprite.png', 'assets/sprite_sheet/json/mec_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 	game.load.atlas('meuf', 'assets/sprite_sheet/meuf_sprite.png', 'assets/sprite_sheet/json/meuf_sprite.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+	
 	game.load.image("table", "assets/table.png");
 	game.load.image("fenetre", "assets/rencontre.png");
 	game.load.image("sol", "assets/background/sol_32x32.png");
